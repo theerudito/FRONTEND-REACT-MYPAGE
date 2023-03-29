c7a0e23c78baac3d575f8f96f2b0389ca9fe3367
theerudito <erudito.tv@gmail.com>
2023-03-29 12:14:29 -0500

    v20

--- src/Complementos/Helpers/ApiRest.jsx
+++ src/Complementos/Helpers/ApiRest.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const backendDev = "http://localhost:5272";
 const backendProduction = "";
 
-const Login = `${backendDev}/Auth/login`;
-const Clients = `${backendDev}/Clients`;
+const Login = `${backendProduction}/Auth/login`;
+const Clients = `${backendProduction}/Clients`;
 
 export const incialValueUser = {
   name: "",
